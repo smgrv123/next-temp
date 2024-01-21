e1a7d5d8a35bfaeae84a83e0c52acb31186809b7
Sumrit Grover <sumritgrover1@gmail.com>
2024-01-21 18:36:38 +0530

    RTK added

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ export default function Home() {
   const { fetchTemplate } = useTemplate();
 
   fetchTemplate();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hellow world
+      hello world
     </main>
   );
 }
