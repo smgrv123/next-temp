55003df52519c614bfa925570584bde3bddef30f
Sumrit Grover <sumritgrover1@gmail.com>
2024-11-11 13:56:22 +0530

    Update - shifted to next 15 - removed redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Provider } from 'react-redux';
-import { store } from '@/src/redux/store';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,9 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Provider store={store}>
-        <body className={inter.className}>{children}</body>
-      </Provider>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
